da9e052b5707a1b441fda72ba70a98bdc7233c04
Andrey <114486619+Shalopai999@users.noreply.github.com>
2024-01-30 23:52:52 +0700

    seminar4

---
+++ seminar4/script.js
@@ -0,0 +1,17 @@
+// // Задача
+// // Необходимо получить список всех пользователей с помощью бесплатного API (https://jsonplaceholder.typicode.com/users) и отобразить их на странице.
+
+// const url = 'https://jsonplaceholder.typicode.com/users';
+
+// const getData = async (URL) => {
+//   const res = await fetch(URL);
+//   const data = await res.json();
+//   return data;
+// };
+
+// try {
+//   const data = await getData(url);
+//   console.log(data);
+// } catch (error) {
+//   console.log(error);
+// }
